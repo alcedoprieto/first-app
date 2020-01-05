95e861d565319d9c48820c1c0798a38729685a2f
alcedoprieto <alcedoprieto@gmail.com>
2020-01-05 12:08:39 -0400

    iniciando el proyecto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+     super(props)
+     this.state = {
+        newTask: "",
+     }
+  }
+
+  handleTaskChange = (event) => {
+    this.setState({
+       newTask: event.target.value,
+    })
+  }
+
+  render() {
+     return (
+        <div className="container">
+           <h1 className="title">
+              Hola Mundo
+              <span aria-label="emoji" role="img">🔥</span>
+           </h1>
+           <input onChange={this.handleTaskChange} type="text" className="new-task"/>
+           <h2 className="test-label">{this.state.newTask}</h2>
+        </div>
+     );
+  }
 }
+
+
 
 export default App;
